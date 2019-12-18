acb4116515e9eb0b17b1024ef4044525baa7badf
abhilashmishra89 <abhilashmishra89@gmail.com>
2019-12-18 16:36:52 +0900

    playing with Jenkinsfile

--- mailer.py
+++ mailer.py
@@ -8,29 +8,28 @@ from email.mime.text import MIMEText
 
 def generateHtml():
 
-    os.system("> disk_check.html")
-    os.system("echo '<html>'>> disk_check.html")
-    os.system("echo '<pre>'>> disk_check.html")
-    os.system("echo '<h2> ==== HDD space status ==== </h2> '> disk_check.html")
+    os.system("> /tmp/disk_check.html")
+    os.system("echo '<html>'>> /tmp/disk_check.html")
+    os.system("echo '<pre>'>> /tmp/disk_check.html")
+    os.system("echo '<h2> ==== HDD space status ==== </h2> '> /tmp/disk_check.html")
     op1 = 'ssh '
     op2 = ' df -h '
-    op3 = '>> disk_check.html'
-    html_file = 'disk_check.html'
-    a = ['C016', 'C019', 'C017', 'C015', 'C010', 'C011', 'C028', 'C027',
-         'C032', 'C034', 'C037', 'C020', 'C031', 'C021', 'C046', 'C030', 'C035']
+    op3 = '>> /tmp/disk_check.html'
+    html_file = '/tmp/disk_check.html'
+    a = myServers
     for i in a:
-        os.system("echo '<pre>'>> disk_check.html")
+        os.system("echo '<pre>'>> /tmp/disk_check.html")
         message = 'echo "<h3>Below is status of : {} </h3>" >> {}'.format(
             i, html_file)
         os.system(message)
         os.system(op1 + i + op2 + op3)
         os.system(
-            "echo '========================================================' >> disk_check.html")
-        os.system("echo '</pre>'>> disk_check.html")
-    os.system("echo '</pre>'>> disk_check.html")
-    os.system("echo '</html>'>> disk_check.html")
+            "echo '========================================================' >> /tmp/disk_check.html")
+        os.system("echo '</pre>'>> /tmp/disk_check.html")
+    os.system("echo '</pre>'>> /tmp/disk_check.html")
+    os.system("echo '</html>'>> /tmp/disk_check.html")
 
-    report_file = open('disk_check.html')
+    report_file = open('/tmp/disk_check.html')
     html = report_file.read()
     msg = MIMEMultipart('alternative')
     msg['Subject'] = "Disk Space Utilization Summary"
