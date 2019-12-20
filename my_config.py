26d8d3be01410bb479c3f55951f6d2fd9f0d902c
abhilashmishra89 <abhilashmishra89@gmail.com>
2019-12-20 14:45:52 +0900

    fromEmail = smtp_id

--- my_config.py
+++ my_config.py
@@ -1,6 +1,6 @@
-smtp_id = 'smtp server address'
-smtp_password = 'smtp password'
 fromEmail = "from@example.com"
-toEmail = ['to@example.com']
+smtp_password = 'smtp password'
+toEmail = ['to-1@example.com', 'to-2@example.com']
+smtp_id = fromEmail
 myServers = ['192.168.1.1', '192.168.1.2', '192.168.1.3',
              '192.168.1.4', '192.168.1.5', '192.168.1.6']
